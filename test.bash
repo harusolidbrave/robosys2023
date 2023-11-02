5c01344c8a474a0673255c92a1306b8f8b6898b6
harusolidbrave <s22c1058qz@s.chibakoudai.jp>
2023-11-02 16:55:43 +0900

    add test

--- test.bash
+++ test.bash
@@ -1,17 +1,3 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: 2022 Ryuichi Ueda
-# SPDX-License-Identifier: BSD-3-Clause
-
-ng () {
-	echo NG at Line $1
-	res=1
-}
-
-res=0
-
-### 1/0 TEST ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 14 ] || ng ${LINENO}
-
-[ "$res" = 0 ] && echo OK
-exit $res
+[ "${out}" = 15.0 ]
