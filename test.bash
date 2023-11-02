3b8f49526a745cc087625cc0de5d7d95f27c03cc
harusolidbrave <s22c1058qz@s.chibakoudai.jp>
2023-11-02 12:23:33 +0900

    add test

--- test.bash
+++ test.bash
@@ -1,5 +1,17 @@
-#!/bin/bash
-
-out=$(seq 5 | ./plus)
-
-[ "${out}" = 15 ]
+#!/bin/bash -xv
+  （略）
+  ### I/O ###
+  out=$(seq 5 | ./plus)
+  [ "${out}" = 15 ] || ng ${LINENO}
+    　 
+  ### STRANGE INPUT ###
+  out=$(echo あ | ./plus)
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
+    　 
+  out=$(echo | ./plus) #空文字
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
+    　 
+[ "$res" = 0 ] && echo OK
+  exit $res
