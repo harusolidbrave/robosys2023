b62ea5fdc8616367c76c7d46e2ebd2e0a52e880f
harusolidbrave <s22c1058qz@s.chibakoudai.jp>
2023-11-02 12:27:39 +0900

    add test

--- test.bash
+++ test.bash
@@ -1,17 +1,15 @@
 #!/bin/bash -xv
-  （略）
-  ### I/O ###
-  out=$(seq 5 | ./plus)
-  [ "${out}" = 15 ] || ng ${LINENO}
-    　 
-  ### STRANGE INPUT ###
-  out=$(echo あ | ./plus)
-  [ "$?" = 1 ]      || ng ${LINENO}
-  [ "${out}" = "" ] || ng ${LINENO}
-    　 
-  out=$(echo | ./plus) #空文字
-  [ "$?" = 1 ]      || ng ${LINENO}
-  [ "${out}" = "" ] || ng ${LINENO}
-    　 
+# SPDX-FileCopyrightText: 2022 Ryuichi Ueda
+# SPDX-License-Identifier: BSD-3-Clause
+### I/O ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+out=$(echo | ./plus) #空文字
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 [ "$res" = 0 ] && echo OK
-  exit $res
+exit $res
